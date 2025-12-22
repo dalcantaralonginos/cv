7d4e9e42ef5a6d6ce2516338cad74862f2f7927b
dalcantaralonginos <dalcantaralonginos@gmail.com>
2025-12-21 18:23:01 -0600

    Update script.js.js

--- script.js.js
+++ script.js.js
@@ -1,9 +1,7 @@
-// Scroll suave
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', e => {
         e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+        document.querySelector(link.getAttribute('href'))
+            .scrollIntoView({ behavior: 'smooth' });
     });
 });
